fd380199677b08667237c0b5217c1e19daa05e9a
SsangSoo <kss10080@naver.com>
2023-07-07 22:26:40 +0900

    write RedisConfig.java with set Password

--- src/main/java/wanderhub/server/config/RedisConfig.java
+++ src/main/java/wanderhub/server/config/RedisConfig.java
@@ -3,7 +3,9 @@ package wanderhub.server.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConfiguration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -18,9 +20,19 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int port;
 
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(redisPassword);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
