04159f7020ff4a308504db7ee538a26a7cdf328d
SsangSoo <kss10080@naver.com>
2023-07-17 17:28:51 +0900

    mytripdetails/update feature complete

--- src/main/java/wanderhub/server/domain/mytrip_plan_detail/mapper/MyTripPlanDetailMapper.java
+++ src/main/java/wanderhub/server/domain/mytrip_plan_detail/mapper/MyTripPlanDetailMapper.java
@@ -1,6 +1,7 @@
 package wanderhub.server.domain.mytrip_plan_detail.mapper;
 
 import org.mapstruct.Mapper;
+import wanderhub.server.domain.mytrip_plan.dto.MyTripPlanDto;
 import wanderhub.server.domain.mytrip_plan_detail.dto.MyTripPlanDetailDto;
 import wanderhub.server.domain.mytrip_plan_detail.entity.MyTripPlanDetail;
 
@@ -20,6 +21,22 @@ public interface MyTripPlanDetailMapper {
                     .whenDate(post.getWhenDate())
                     .timeStart(post.getTimeStart())
                     .timeEnd(post.getTimeEnd())
+                    .build();
+        }
+        return null;
+    }
+
+    default MyTripPlanDetail myTripPlanDetailPatchDtoToEntity(MyTripPlanDetailDto.Patch patch) {
+        if(Objects.nonNull(patch)) {
+            return MyTripPlanDetail.builder()
+                    .subTitle(patch.getSubTitle())
+                    .content(patch.getContent())
+                    .coordinateX(patch.getCoordinateX())
+                    .coordinateY(patch.getCoordinateY())
+                    .placeName(patch.getPlaceName())
+                    .whenDate(patch.getWhenDate())
+                    .timeStart(patch.getTimeStart())
+                    .timeEnd(patch.getTimeEnd())
                     .build();
         }
         return null;
--- src/main/java/wanderhub/server/domain/mytrip_plan_detail/service/MyTripPlanDetailService.java
+++ src/main/java/wanderhub/server/domain/mytrip_plan_detail/service/MyTripPlanDetailService.java
@@ -10,6 +10,11 @@ import wanderhub.server.domain.mytrip_plan_detail.dto.MyTripPlanDetailResponseDt
 import wanderhub.server.domain.mytrip_plan_detail.entity.MyTripPlanDetail;
 import wanderhub.server.domain.mytrip_plan_detail.repository.MyTripPlanDetailQueryDsl;
 import wanderhub.server.domain.mytrip_plan_detail.repository.MyTripPlanDetailRepository;
+import wanderhub.server.global.exception.CustomLogicException;
+import wanderhub.server.global.exception.ExceptionCode;
+import wanderhub.server.global.utils.CustomBeanUtils;
+
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -19,14 +24,17 @@ public class MyTripPlanDetailService {
     private final MyTripPlanService myTripPlanService;
     private final MyTripPlanDetailQueryDsl myTripPlanDetailQueryDsl;
     private final MyTripPlanDetailRepository myTripPlanDetailRepository;
+    private final CustomBeanUtils<MyTripPlanDetail> customBeanUtils;
 
-    public MyTripPlanDetailService(MemberService memberService, MyTripPlanService myTripPlanService, MyTripPlanDetailQueryDsl myTripPlanDetailQueryDsl, MyTripPlanDetailRepository myTripPlanDetailRepository) {
+    public MyTripPlanDetailService(MemberService memberService, MyTripPlanService myTripPlanService, MyTripPlanDetailQueryDsl myTripPlanDetailQueryDsl, MyTripPlanDetailRepository myTripPlanDetailRepository, CustomBeanUtils<MyTripPlanDetail> customBeanUtils) {
         this.memberService = memberService;
         this.myTripPlanService = myTripPlanService;
         this.myTripPlanDetailQueryDsl = myTripPlanDetailQueryDsl;
         this.myTripPlanDetailRepository = myTripPlanDetailRepository;
+        this.customBeanUtils = customBeanUtils;
     }
 
+    // 개인 일정 세부 생성
     public MyTripPlanDetailResponseDto createTripPlanDetail(String email, Long myTripPlanId, MyTripPlanDetail myTripPlanDetail) {
         Member findMember = memberService.findMember(email);
         memberService.verificationMember(findMember);       // 통과시 회원 검증 완료
@@ -35,4 +43,26 @@ public class MyTripPlanDetailService {
         MyTripPlanDetail createdTripPlanDetail = myTripPlanDetailRepository.save(myTripPlanDetail);
         return myTripPlanDetailQueryDsl.getMyTripPlanDetail(createdTripPlanDetail.getMyTripPlanDetailId());
     }
+
+    public MyTripPlanDetailResponseDto updateTripPlanDetail(String email, Long myTripPlanId, Long myTripPlanDetailId, MyTripPlanDetail patchToMyTripPlanDetail) {
+        Member findMember = memberService.findMember(email);
+        memberService.verificationMember(findMember);       // 통과시 회원 검증 완료
+        MyTripPlan myTripPlan = myTripPlanService.verificationMyTrip(myTripPlanId, findMember.getNickName()); // MyTripPlan 확인
+        // 부모와 자식 Id가 맞는 MyTripPlanDetail 확인
+        MyTripPlanDetail byMyTripPlanDetailId = findByMyTripPlanDetailId(myTripPlanDetailId, myTripPlanId);
+        MyTripPlanDetail updatedMyTripPlanDetail = customBeanUtils.copyNonNullProoerties(patchToMyTripPlanDetail, byMyTripPlanDetailId);
+        return myTripPlanDetailQueryDsl.getMyTripPlanDetail(updatedMyTripPlanDetail.getMyTripPlanDetailId());
+    }
+
+
+
+
+    //--------------유효성 검증------------------
+
+    // MyTripPlanDetail이 있는지 확인
+    public MyTripPlanDetail findByMyTripPlanDetailId(Long myTripPlanDetailId, Long myTripPlanId) {
+        Optional<MyTripPlanDetail> byMyTripPlanDetailId = myTripPlanDetailRepository.findByMyTripPlanDetailId(myTripPlanDetailId, myTripPlanId);
+        return byMyTripPlanDetailId.orElseThrow(() -> new CustomLogicException(ExceptionCode.TRIP_PLAN_DETAIL_NOT_FOUND));
+    }
+
 }
